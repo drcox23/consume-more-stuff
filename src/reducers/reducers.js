667774bf37acf4fcf92ee62175664742cc0f6977
doug <drcox23@gmail.com>
2018-11-08 19:45:28 -1000

    basic redux setup, will need to test after server.js is setup

---
+++ src/reducers/reducers.js
@@ -0,0 +1,22 @@
+import {
+  GET_ALL_POSTS,
+  GET_ALL_APPROVED_COMMENTS,
+  GET_PENDING_COMMENTS,
+  ADD_POST,
+  ADD_COMMENT
+}
+from '../actions/actions.js'
+
+const postReducer = (state =[], action) => {
+  console.log('REDUCER ACTION');
+
+  switch (action.type) {
+    case GET_ALL_POSTS:
+      console.log('GET_ALL_TASKS reducer')
+      return action.payload
+    default:
+      return state
+  }
+}
+
+export default postReducer
