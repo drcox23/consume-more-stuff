ed3c6d11b950998f4b3d9722f8a1bd924e3e0041
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-09 01:21:46 -1000

    Redid home page html and css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+//CLIENT
+
 import React, { Component } from 'react';
 import logo from './2cents.png';
 import './App.css';
@@ -11,64 +13,107 @@ import PostsBoard from './components/PostsBoard/PostsBoard.jsx';
 //Setup for redux
 import { connect } from 'react-redux';
 
-// const bodyStyle = {
-//   backgroundColor: 'lightcoral',
-//   width: '100%',
-//   height: '100%',
-//   margin: '0'
-// }
-
-// const imgStyle = {
-//   margin: '30px',
-//   width: '100px',
-// }
-
-// // const navStyle = {
-// //   width: '100%',
-// // }
-
-// const loginBtnStyle = {
-//   width: '120px',
-//   margin: '50px 0',
-//   marginRight: '20px',
-//   height: '30px',
-//   fontSize: '1.2em',
-//   float: 'right',
-//   hover: 
-// }
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  //~~~~~~~~Lifecycle Methods~~~~~~~~~//
+  componentDidMount() {
+
+  }
+
+  //~~~~~~~~App Component Methods~~~~~~~~~//
+
+
+
+  //~~~~~~~~App Component - RENDER~~~~~~~~~//
   render() {
     return (
       <div className="App">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-        </link>
+        {/* Fonts */}
+        <style>
 
+        </style>
+
+        {/* Header */}
         <header className="App-header">
+          {/* <div id="headerBar">
+            <img src={logo} alt="logo" />
+            <input id="searchBar" type="text" placeholder="Search..." />
 
-          {/* Navigation */}
-          <img src={logo} alt="logo" />
+          </div> */}
+
+          {/* Routing Links & Routes */}
+          <Router>
+            <div id="navbar">
+
+              <div id="headerBar">
+                <img src={logo} alt="logo" />
+                <input id="searchBar" type="text" placeholder="Search..." />
+
+
+                <Link to="/">
+                  <button id="homeBtn" type="button">Home</button>
+                </Link>
+
+                <Link to="/login">
+                  <button id="loginBtn" type="button">Log In</button>
+                </Link>
+
+                <Link to="/signup">
+                  <button id="signupBtn" type="button">Sign Up</button>
+                </Link>
+
+                <Route path="/">
+
+                </Route>
+
+                <Route path="/login">
+
+                </Route>
+
+                <Route path="/signup">
+
+                </Route>
+              </div>
+            </div>
+          </Router>
         </header>
-        <Router>
-          <div>
 
-            {/* Routing Links and Routes */}
 
-            <Link to="/">
-              <button type="button">Home</button>
-            </Link>
 
-            <Link to="/login">
-              <button type="button">Login <i class="fas fa-sign-in-alt"></i></button>
-            </Link>
-
-            <Route exact path="/" component={PostsBoard} />
-            <Route path="/login" component={Login} />
-            {/* <Route path="/login" /> */}
-
-          </div>
-        </Router>
       </div>
+
+      // <div className="App">
+      //   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+      //   </link>
+
+      //   <header className="App-header">
+
+      //     {/* Navigation */}
+      //     <img src={logo} alt="logo" />
+      //   </header>
+      //   <Router>
+      //     <div>
+
+      //       {/* Routing Links and Routes */}
+
+      //       <Link to="/">
+      //         <button type="button">Home</button>
+      //       </Link>
+
+      //       <Link to="/login">
+      //         <button type="button">Login <i class="fas fa-sign-in-alt"></i></button>
+      //       </Link>
+
+      //       <Route exact path="/" component={PostsBoard} />
+      //       <Route path="/login" component={Login} />
+      //       {/* <Route path="/login" /> */}
+
+      //     </div>
+      //   </Router>
+      // </div>
     );
   }
 }
