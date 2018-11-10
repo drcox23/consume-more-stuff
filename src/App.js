5ca79f51c470fe78ae6fc2389a5a9f847530188a
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-10 13:06:29 -1000

    Placed header components in correct places

--- src/App.js
+++ src/App.js
@@ -18,18 +18,21 @@ import { connect } from 'react-redux';
 const Header = (props) => {
   return (
     <div id="headerBar">
-      {/* imgBox is for resizing the logo with a fixed scale */}
-      <div id="imgBox">
-        <img src={logo} alt="logo" />
-      </div>
 
-      {/* Search Bar */}
-      <input id="searchBar" type="text" placeholder="Search..." />
+      <div id="subheader-container">
+        {/* imgBox is for resizing the logo with a fixed scale */}
+        <div id="imgBox">
+          <img src={logo} alt="logo" />
+        </div>
+
+        {/* Search Bar */}
+        <input id="searchBar" type="text" placeholder="Search..." />
+      </div>
 
       {/* Navigation Links */}
       {props.children}
-
     </div>
+
   )
 }
 
