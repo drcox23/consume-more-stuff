60b83dd4a11503a4c4acb451e479270798b581e0
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-10 19:38:01 -1000

    Refactored Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -10,9 +10,7 @@ class Home extends Component {
       <div className="container">
         {
           isAuthenticated() && (
-            <h4>
-              You are logged in!
-              </h4>
+            <h4>You are logged in!</h4>
           )
         }
         {
