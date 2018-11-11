db4c989e9907115f1f43d550537714fd5197d2b9
doug <drcox23@gmail.com>
2018-11-10 16:32:46 -1000

    draft posts logic has been setup

--- server/server.js
+++ server/server.js
@@ -42,17 +42,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use('/', router)
 
 
 // get all the posts when any user lands on home page
-app.get('/home', (req, res) => {
+router.get('/home', (req, res) => {
   Posts
     .fetchAll()
     .then(items => {
-      res.json(items.serialize())
+      res.json(items)
     })
     .catch(err => {
-      console.log('error', err)
+      console.log("home get error", err)
+      res.json(err)
     })
 });
 
@@ -62,7 +64,9 @@ router.get('/post/:id', (req, res) => {
   console.log('whats the id', id);
 
   Posts
-    .where({id})
+    .where({
+      id
+    })
     .fetch()
     .then(results => {
       const posts = results.toJSON()
@@ -73,15 +77,17 @@ router.get('/post/:id', (req, res) => {
     .catch(err => {
       console.log("post by id error", err)
     })
-  
+
 })
 
 // get the comments associated with a post
 router.get('/comments/:id', (req, res) => {
   const id = req.params.id;
-  
+
   Comments
-    .where({post_id: id})
+    .where({
+      post_id: id
+    })
     .fetchAll()
     .then(results => {
       const comments = results.toJSON()
@@ -98,7 +104,9 @@ router.get('/mycomments/:id', (req, res) => {
   const id = req.params.id;
 
   Comments
-    .where({user_id: id})
+    .where({
+      user_id: id
+    })
     .fetchAll()
     .then(results => {
       const comments = results.toJSON()
@@ -110,23 +118,115 @@ router.get('/mycomments/:id', (req, res) => {
     })
 })
 
-router.post('/add', (req, res) => {
-  const post_data = req.body
-  console.log("post data we are adding to DB", req.body)
+router.route('/add')
+  .post((req, res) => {
+    const post_data = req.body
+    console.log("post data we are adding to DB", req.body)
 
-  Posts
-    .forge(post_data)
-    .save()
-    .then(results => {
-      return Posts.fetchAll()
+    Posts
+      .forge(post_data)
+      .save()
+      .then(results => {
+        return Posts.fetchAll()
+      })
+      .then(results => {
+        res.json(results.serialize())
+      })
+      .catch(err => {
+        console.log("server post error", err)
+      })
+  });
+
+router.get('/drafts/:id', (req, res) => {
+  const id = req.params.id
+
+  draftPosts
+    .where({
+      user_id: id
     })
+    .fetchAll()
     .then(results => {
-      res.json(results.serialize())
+      const draftPosts = results.toJSON()
+      res.json(draftPosts)
     })
     .catch(err => {
-      console.log("server post error", err)
+      console.log("get all drafts error", err)
     })
-})
+});
+
+router.route('/drafts/:id/:draftId')
+  .get((req, res) => {
+    const id = req.params.id //supposed to be for userid
+    const draftId = req.params.draftId //post draft id
+
+    draftPosts
+      .where({
+        user_id: id,
+        id: draftId
+      })
+      .fetch()
+      .then(results => {
+        const draftPost = results.toJSON()
+        res.json(draftPost)
+      })
+      .catch(err => {
+        console.log("drafts by id error", err)
+      })
+  })
+
+
+  .put((req, res) => {
+
+    const draftId = req.params.draftId //post draft id
+
+    const postDraft_data = req.body
+    console.log("post data we are adding to DB", req.body)
+
+    draftPosts
+      .where({
+        id: draftId
+      })
+      .fetch()
+      .then(update => {
+        return update.save(postDraft_data)
+      })
+      .then(data => {
+        return draftPosts.where({
+          id: draftId
+        }).fetch();
+      })
+      .then(results => {
+        res.json(results)
+      })
+      .catch(err => {
+        console.log("draftPost error", err)
+        res.json(err)
+      })
+
+  })
+
+  .post((req, res) => {
+    const post_data = req.body
+    const draftId = req.params.draftId //post draft id
+
+    // console.log("post data we are adding to DB", req.body)
+
+    Posts
+      .forge(post_data)
+      .save()
+      .then(results => {
+        return Posts.fetchAll()
+      })
+      .then(results => {
+        res.json(results)
+      })
+      .then( () => {
+        draftPosts.where({id: draftId}).destroy()
+      })
+      .catch(err => {
+        console.log("server post error", err)
+      })
+  })
 
 // get the post by id along with the ALL the comments associated with it. 
 // app.get('/post/:id', (req, res) => {
