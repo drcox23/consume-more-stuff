302f48967f4ea59fb328dd56b309a85b5d46ad82
Wymin <wyminc808@gmail.com>
2018-11-13 22:57:10 -1000

    Added password column and seed data for passwords on users table

--- server/knex/migrations/20181108104653_create_users_table.js
+++ server/knex/migrations/20181108104653_create_users_table.js
@@ -3,6 +3,8 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('users', function (table) {
       table.increments();
       table.string('username').notNullable();
+      table.unique('username');
+      table.string('password').notNullable();
       table.string('email').notNullable();
       table.string('first_name').notNullable();
       table.string('last_name').notNullable();
