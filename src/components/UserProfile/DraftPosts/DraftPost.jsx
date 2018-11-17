123b0f8a3fe09adfb65216914d0fb24e7ac1b830
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-17 11:50:05 -1000

    Added an EditForm and added an edit and delete button on draft posts

--- src/components/UserProfile/DraftPosts/DraftPost.jsx
+++ src/components/UserProfile/DraftPosts/DraftPost.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './DraftPost.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const DraftPosts = (props) => {
-    return props.draftPosts.map(draftPost => 
-        <div key={draftPost.id} className="posting">
-            <p>{draftPost.subject}</p>
+    return props.draftPosts.map(draftPost =>
+        <div key={draftPost.id} className="draftPost">
+            <p className="draftPost-subject">{draftPost.subject}</p>
+            <Link to="/editForm">
+                <button id="editBtn" type="button">Edit Draft</button>
+            </Link>
+            <Link to="/user/profile">
+                <button id="deleteBtn" type="button">Delete Draft</button>
+            </Link>
         </div>
     )
 }
