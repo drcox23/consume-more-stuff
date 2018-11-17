55aadab49460ca0fb05cacc33d8f5e521b06ace9
Chaz Kondo <Chaz@Macbook-Pro-2.local>
2018-11-17 07:25:00 -1000

    Should redirect auth0 properly

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,14 +1,14 @@
-export const AUTH_CONFIG = {
-    domain: 'twocentsforyou.auth0.com',
-    clientId: 'xcOHO3wbcR5HpAtMxwW419j5K7ijjOAE',
-    callbackUrl: 'http://localhost:3000/callback'
-  }
+// export const AUTH_CONFIG = {
+//     domain: 'twocentsforyou.auth0.com',
+//     clientId: 'xcOHO3wbcR5HpAtMxwW419j5K7ijjOAE',
+//     callbackUrl: 'http://localhost:3000/callback'
+//   }
 
 
   // S3 build
 
-  // export const AUTH_CONFIG = {
-  //   domain: 'twocentsforyou.auth0.com',
-  //   clientId: 'xcOHO3wbcR5HpAtMxwW419j5K7ijjOAE',
-  //   callbackUrl: 'http://mytwocents.s3-website-us-west-2.amazonaws.com/callback'
-  // }
+  export const AUTH_CONFIG = {
+    domain: 'twocentsforyou.auth0.com',
+    clientId: 'xcOHO3wbcR5HpAtMxwW419j5K7ijjOAE',
+    callbackUrl: 'http://mytwocents.s3-website-us-west-2.amazonaws.com/callback'
+  }
