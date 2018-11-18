07f483a8548279d66129a23646a97be4122d9507
doug <drcox23@gmail.com>
2018-11-17 18:42:11 -1000

    fixed draft post to show up

--- src/components/UserProfile/DraftPosts/DraftPost.jsx
+++ src/components/UserProfile/DraftPosts/DraftPost.jsx
@@ -3,6 +3,7 @@ import './DraftPost.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const DraftPosts = (props) => {
+    console.log("POST PROPS", props)
     return props.draftPosts.map(draftPost =>
         <div key={draftPost.id} className="draftPost">
             <p className="draftPost-subject">{draftPost.subject}</p>
