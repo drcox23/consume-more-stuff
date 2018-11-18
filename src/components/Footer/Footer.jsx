122727fb9b15102eb80927df3a13505355a38eda
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-17 15:28:34 -1000

    Fixed footer css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,9 +8,10 @@ class Footer extends Component {
       <footer>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossOrigin="anonymous">
         </link>
-        <i className="fab fa-github-alt fa-2x">
-          <a href="https://github.com/maymc/consume-more-stuff" target="_blank"> Visit our Github!</a>
-        </i>
+
+        <a href="https://github.com/maymc/consume-more-stuff" target="_blank">
+          <i className="fab fa-github-alt fa-2x"></i>
+        </a>
       </footer>
     );
   }
