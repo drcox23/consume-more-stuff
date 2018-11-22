bb936f869643c39912f162d686de1afcbe5b96e7
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-21 20:54:41 -1000

    Changed code for my-posts so that my posts are clickable

--- src/components/UserProfile/MyPosts/MyPosts.jsx
+++ src/components/UserProfile/MyPosts/MyPosts.jsx
@@ -2,53 +2,51 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './MyPosts.css';
 import { connect } from 'react-redux';
-import { getAllPosts } from '../../../actions/actions.js';
+import Posts from '../../PostsBoard/posts/posts.jsx'
+import { getPostandCommentsById, getAllPosts } from '../../../actions/actions.js';
 
-const MyPosts = (props) => {
-  console.log("My Posts - props.props:", props.props);
+class MyPosts extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return props.props.items.filter(myPost => props.props.user.id === myPost.id).map(myPost =>
-    <div key={myPost.id} className="myPost-container">
-      <p className="myPost-subject">{myPost.subject}</p>
-      <button>Archive</button>
+  getPostandCommentsById = (props) => {
+    this.props.dispatch(
+      getPostandCommentsById(props),
+    )
+  }
 
-    </div>
+  checkForParentComponent = () => {
+    if (this.props.auth) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-  )
+  render() {
+    const user = this.props.user;
+    const auth = this.props.auth;
+    const items = this.props.items.filter(posts => user.id === posts.user_id);
+    console.log("MyPosts - items:", items);
+    console.log("user.id:", user.id);
+    console.log("")
+
+    return (
+      <div>
+        <Posts props={this.props} auth={auth} items={items} getPostandCommentsById={this.getPostandCommentsById} />
+      </div>
+    )
+  }
+
 }
 
+const mapStateToProps = state => {
+  return {
+    items: state.items,
+    user: state.user
+  }
+}
 
-export default MyPosts
-
-
-
-// class MyPosts extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   componentDidMount = () => {
-//     this.props.dispatch(getAllPosts());
-//   }
-
-//   render() {
-//     const { items } = this.props;
-//     console.log("what is items??: ", items);
-
-//     return (
-//       <div>{items.map}</div>
-//     )
-
-
-
-//   }
-// }
-
-
-// const mapStateToProps = state => {
-//   return {
-//     items: state.items
-//   }
-// }
-
-// export default connect(mapStateToProps)(MyPosts);
+export default connect(mapStateToProps)(MyPosts);
 
