ee1dea7a4283b00c65c45188ef96f6a7357b3018
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-24 01:34:26 -1000

    Fixed css for edit form for draft

--- src/components/forms/EditDraftPostForm.jsx
+++ src/components/forms/EditDraftPostForm.jsx
@@ -11,18 +11,18 @@ class EditDraftPostForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        user_id: "",
-        subject: "",
-        body: "",
-        type_id: "",
-        price: ""
+      user_id: "",
+      subject: "",
+      body: "",
+      type_id: "",
+      price: ""
     }
   }
 
   //This will set state from props everytime props changes
-  static getDerivedStateFromProps(nextProps, prevState){
-    if(nextProps.detailedDraftPost !== prevState.detailedDraftPost){
-      return {         
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.detailedDraftPost !== prevState.detailedDraftPost) {
+      return {
         original: {
           user_id: nextProps.user.id,
           subject: nextProps.detailedDraftPost.subject,
@@ -31,12 +31,12 @@ class EditDraftPostForm extends Component {
           price: nextProps.detailedDraftPost.price
         }
       };
-   }
-   else return null;
+    }
+    else return null;
   }
 
   componentDidMount() {
-    const  { name } = jwtDecode(localStorage.getItem('id_token'))
+    const { name } = jwtDecode(localStorage.getItem('id_token'))
     const { id } = this.props.match.params
 
     this.props.dispatch(
@@ -61,7 +61,7 @@ class EditDraftPostForm extends Component {
   submittingForm = () => {
     let form = {};
 
-    for(const key in this.state) {
+    for (const key in this.state) {
       if (this.state[key] === "") {
         form[key] = this.state.original[key]
       } else {
@@ -88,72 +88,71 @@ class EditDraftPostForm extends Component {
   }
 
   DefaultType = () => {
-    if(!this.props.detailedDraftPost.type_id) {
+    if (!this.props.detailedDraftPost.type_id) {
       return <option>Select Media Type...</option>
     } else {
       return (<option key={this.props.detailedDraftPost.type_id} value={this.props.detailedDraftPost.type_id}>
         {this.props.type.filter(type => type.id === this.props.detailedDraftPost.type_id)[0].type}
-        </option>)
+      </option>)
     }
   }
 
   SelectType = () => {
-    if(!this.props.detailedDraftPost.type_id) {
+    if (!this.props.detailedDraftPost.type_id) {
       return this.props.type.map(line => <option key={line.id} value={line.id}>{line.type}</option>)
     } else {
-      return (this.props.type.filter(type=> type.id !== this.props.detailedDraftPost.type_id).map(line => <option key={line.id} value={line.id}>{line.type}</option>))
+      return (this.props.type.filter(type => type.id !== this.props.detailedDraftPost.type_id).map(line => <option key={line.id} value={line.id}>{line.type}</option>))
     }
   }
 
   render() {
     console.log(this.props)
     return (
-      <div id="container">
+      <div id="edit-draft-post-container">
 
-        <div id="new-request-title">Edit Draft Post</div>
+        <div id="edit-draft-post-title">Edit Draft Post</div>
 
         {/* New Request form */}
         <form onSubmit={this.handleSubmit}>
 
-          <div class="row">
-            <div class="rowHeader">
-              <label>Subject:</label>
-              <input onChange={this.handleChange} className="user-input" type="text" name="subject" defaultValue={this.props.detailedDraftPost.subject} />
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="rowHeader">
-              <label>Body:</label>
-              <input onChange={this.handleChange} className="user-input" type="text" name="body" defaultValue={this.props.detailedDraftPost.body} />
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="rowHeader">
-              <label>Media Type:
+          {/* <div class="row"> */}
+          {/* <div class="rowHeader"> */}
+          <label className="edit-draft-post-label">Subject:</label>
+          <input onChange={this.handleChange} className="user-edit-draft-post-input" type="text" name="subject" defaultValue={this.props.detailedDraftPost.subject} />
+          {/* </div> */}
+          {/* </div> */}
+          <br />
+          {/* <div class="row"> */}
+          {/* <div class="rowHeader"> */}
+          <label className="edit-draft-post-label">Body:</label>
+          <input onChange={this.handleChange} className="user-edit-draft-post-input" type="text" name="body" defaultValue={this.props.detailedDraftPost.body} />
+          {/* </div> */}
+          {/* </div> */}
+          <br />
+          {/* <div class="row"> */}
+          {/* <div class="rowHeader"> */}
+          <label className="edit-draft-post-label">Media Type:
                 <select onChange={this.handleChange} name="type_id">
-                  {this.DefaultType()}
-                  {this.SelectType()}
-                </select>
-              </label>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="rowHeader">
-              <label>Set a Price:</label>
-              <input onChange={this.handleChange} className="user-input" type="text" name="price" defaultValue={this.props.detailedDraftPost.price} />
-            </div>
-          </div>
+              {this.DefaultType()}
+              {this.SelectType()}
+            </select>
+          </label>
+          {/* </div> */}
+          {/* </div> */}
+          <br />
+          {/* <div class="row"> */}
+          {/* <div class="rowHeader"> */}
+          <label className="edit-draft-post-label">Set a Price:</label>
+          <input onChange={this.handleChange} className="user-edit-draft-post-input" type="text" name="price" defaultValue={this.props.detailedDraftPost.price} />
+          {/* </div> */}
+          {/* </div> */}
 
           <br />
-          <input id="user-newReq-btn" type="submit" value="Submit new request" onClick={this.addToPosts}/>
-          <br /><br />
-          <input id="user-save-draft-btn" type="submit" value="Save draft for later" onClick={this.editToDraftPosts}/>
+          <input id="user-newReq-btn" type="submit" value="Submit new request" onClick={this.addToPosts} />
+          <input id="user-save-draft-btn" type="submit" value="Save draft for later" onClick={this.editToDraftPosts} />
           <br />
         </form>
-      </div>
+      </div >
     );
   }
 }
