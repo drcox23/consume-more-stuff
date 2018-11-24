d14354aa4a6fa278d23e4516768b027634f07451
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-24 10:09:44 -1000

    Working on updating css for my comments

--- src/components/PostDetail/comments/MyComments.jsx
+++ src/components/PostDetail/comments/MyComments.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './MyComments.css';
 
 const LinkButton = (props) => {
     return (
-      <Link to={props.to}>
-        {/* <button className="auth-navbar-btns">{props.title}</button> */}
-        <p className="auth-navbar-btns">{props.title}</p>
-      </Link>
+        <Link to={props.to}>
+            {/* <button className="auth-navbar-btns">{props.title}</button> */}
+            <p className="auth-navbar-btns">{props.title}</p>
+        </Link>
     )
-  }
+}
 
 export const MyComments = (props) => {
-    return (props.userComments.map(comment =>
-        {let selectedComment = comment.post_id;
-        return (<div key={comment.id} className="comment-container">
-            <div className="post-id">Post: {props.items.map(post => 
-                {if (post.id === selectedComment)
-                return (<LinkButton key={post.id} to={`/dashboard/post/${post.id}`} title={post.subject} />)})}</div>
-            <p className="comment-user-id">User: {comment.user_id}</p>
-            <p className="comment-body">{comment.body}</p>
-            <p className="comment-created-timestamp">created:<br />{comment.created_at}</p>
-            <p className="comment-updated-timestamp">last update:<br />{comment.created_at}</p>
-                </div>)})    
+    return (
+
+        props.userComments.map(comment => {
+
+            let selectedComment = comment.post_id;
+
+            return (
+                <div key={comment.id} className="my-comment-bubble">
+                    <div className="post-id">Post: {props.items.map(post => {
+                        if (post.id === selectedComment)
+                            return (
+                                <LinkButton key={post.id} to={`/dashboard/post/${post.id}`} title={post.subject} />
+                            )
+                    })}
+                    </div>
+
+                    <p className="comment-user-id">User: {comment.user_id}</p>
+
+                    <p className="comment-body">{comment.body}</p>
+
+                    <p className="comment-created-timestamp">created:<br />{comment.created_at}</p>
+
+                    <p className="comment-updated-timestamp">last update:<br />{comment.created_at}</p>
+                </div>
+            )
+        })
     )
 }
 
